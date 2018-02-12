a0583e685aa75e0377dd982f48eafafe20ba8e01
albert95014 <albertya@andrew.cmu.edu>
2018-02-11 22:46:53 -0500

    Assignment 2, Part 2

--- convert.py
+++ convert.py
@@ -1,11 +1,13 @@
+import json
 import cc_dat_utils
+import test_json_utils
 
 #Part 1
 #Use cc_data_utils.make_cc_data_from_dat() to load pfgd_test.dat
 #print the resulting data
 
-print("hi")
-print(cc_dat_utils.make_cc_data_from_dat("data/pfgd_test.dat"))
+# print("hi")
+# print(cc_dat_utils.make_cc_data_from_dat("data/pfgd_test.dat"))
 
 
 
@@ -15,8 +17,15 @@ input_json_file = "data/test_data.json"
 ### Begin Add Code Here ###
 #Open the file specified by input_json_file
 #Use the json module to load the data from the file
+with open(input_json_file, "r") as reader:
+    game_data = json.load(reader)
+print(game_data)
+
 #Use make_game_library_from_json(json_data) to convert the data to GameLibrary data
+game_library_data = test_json_utils.make_game_library_from_json(game_data)
+
 #Print out the resulting GameLibrary data using print_game_library(game_library_data) in test_data.py
+print(game_library_data)
 ### End Add Code Here ###
 
 
--- test_json_utils.py
+++ test_json_utils.py
@@ -1,17 +1,48 @@
-import test_data
+import sys
 import json
+import test_data
+
+
 
 #Creates and returns a GameLibrary object(defined in test_data) from loaded json_data
-def make_game_library_from_json( json_data ):
+def make_game_library_from_json(json_data):
     #Initialize a new GameLibrary
     game_library = test_data.GameLibrary()
 
     #Loop through the json_data
+    for game_data in json_data["Games"]:
+
         #Create a new Game object from the json_data by reading
+        game = test_data.Game()
+
         #  title
+        game.title = game_data["title"]
+
         #  year
+        game.year = game_data["year"]
+
         #  platform (which requires reading name and launch_year)
+        for platform_data in game_data["platform"]:
+            game.platform = test_data.Platform()
+            game.platform.launch_year = game_data["platform"]["launch year"]
+            game.platform.name = game_data["platform"]["name"]
+
         #Add that Game object to the game_library
+        game_library.add_game(game)
+
+
+
+
+
+
+
+
+
+
+        print("hello")
+
+
     #Return the completed game_library
+
 
     return game_library
